f92597b6b2814012e00f62947fc9f2ba1082414d
Patrick Niezen <patrickniezen@gmail.com>
2017-04-28 14:20:31 +0200

    Polling mechanism now updates data-attributes and badge content based on new rankings.

--- src/SlimsteVriend.Web/wwwroot/scripts/app.js
+++ src/SlimsteVriend.Web/wwwroot/scripts/app.js
@@ -1,7 +1,17 @@
 ﻿$(document).ready(function () {
     setInterval(function () {
         $.get("api/ranking", function (data) {
-            console.log(data);
+            // Update data-attributes and badge with new ranking
+            $('#ranking li').each(function () {
+                var candidate = $(this).attr('data-candidate');
+                var newPosition = data.indexOf(candidate);
+
+                $(this).attr('data-position', newPosition);
+                $('.badge', this).html(newPosition + 1);
+            });
+
+            // Animate candidates to new positions based on ranking
+
         });
     }, 1000);
 });
