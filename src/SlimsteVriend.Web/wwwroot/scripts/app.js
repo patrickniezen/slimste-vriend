1c17a11a0a5fba9739e16bb38eed4163a0423b48
Patrick Niezen <patrickniezen@gmail.com>
2017-04-28 16:01:34 +0200

    Ranking now shows animation when candidates swap rank.

--- src/SlimsteVriend.Web/wwwroot/scripts/app.js
+++ src/SlimsteVriend.Web/wwwroot/scripts/app.js
@@ -1,17 +1,24 @@
 ﻿$(document).ready(function () {
+    // Fetch and sync ranking each X miliseconds
     setInterval(function () {
         $.get("api/ranking", function (data) {
-            // Update data-attributes and badge with new ranking
             $('#ranking li').each(function () {
                 var candidate = $(this).attr('data-candidate');
+
+                var oldPosition = $(this).attr('data-rank');
                 var newPosition = data.indexOf(candidate);
 
-                $(this).attr('data-position', newPosition);
+                // Animate candidates to new positions based on ranking if needed.
+                if (oldPosition != newPosition) {
+                    var elementHeight = $(this).outerHeight();
+                    var deltaY = (newPosition - oldPosition) * elementHeight;
+                    $(this).animate({ "top": '+=' + deltaY }, 1000);
+                }
+
+                // Update data-attributes and badge with new ranking
+                $(this).attr('data-rank', newPosition);
                 $('.badge', this).html(newPosition + 1);
             });
-
-            // Animate candidates to new positions based on ranking
-
         });
-    }, 1000);
+    }, 2500);
 });
